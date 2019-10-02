3a3f4d16f6207dfa4f3817f533e69e6bf7542bfc
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-10-02 13:59:18 +0300

    Fix import

--- server/main.go
+++ server/main.go
@@ -4,15 +4,22 @@ import (
 	"fmt"
 
 	"github.com/fr3fou/aumo/server/aumo"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello")
+	db, err := gorm.Open("mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
 	a := aumo.New(aumo.Config{})
 
 	fmt.Printf("%+v", a)
-	err := godotenv.Load()
+	err = godotenv.Load()
 	if err != nil {
 		panic(".env file not found")
 	}
