fd96b35a9f71839babb9602b28d0b4d31fe6b75e
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-10-02 09:56:50 +0300

    aumo domain

--- server/aumo/aumo.go
+++ server/aumo/aumo.go
@@ -1,1 +1,15 @@
 package aumo
+
+import "github.com/jinzhu/gorm"
+
+type Config struct {
+	DB *gorm.DB
+}
+
+type Aumo struct {
+	config Config
+}
+
+func New(c Config) Aumo {
+	return Aumo{config: c}
+}
