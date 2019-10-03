8940d573fa3b95407249b2ac1f7b79b33e0897d1
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-10-03 10:25:06 +0300

    Format

--- server/main.go
+++ server/main.go
@@ -16,9 +16,16 @@ func main() {
 		panic(".env file not found")
 	}
 
-	MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOST, MYSQL_PORT := os.Getenv("MYSQL_DATABASE"), os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"), os.Getenv("MYSQL_PORT")
+	MYSQL_DATABASE := os.Getenv("MYSQL_DATABASE")
+	MYSQL_USER := os.Getenv("MYSQL_USER")
+	MYSQL_PASSWORD := os.Getenv("MYSQL_PASSWORD")
+	MYSQL_HOST := os.Getenv("MYSQL_HOST")
+	MYSQL_PORT := os.Getenv("MYSQL_PORT")
+
 	MYSQL_STRING := MYSQL_USER + ":" + MYSQL_PASSWORD + "@(" + MYSQL_HOST + ":" + MYSQL_PORT + ")/" + MYSQL_DATABASE
+
 	db, err := gorm.Open("mysql", MYSQL_STRING)
+
 	if err != nil {
 		panic(err)
 	}
