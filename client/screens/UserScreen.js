e4a1d478413bf5e979dcf578f4d6c0f456f63ae9
Simo Aleksandrov <simo3003@me.com>
2019-10-05 21:18:13 +0300

    fetch user on user page

--- client/screens/UserScreen.js
+++ client/screens/UserScreen.js
@@ -1,7 +1,28 @@
+import axios from "axios"
+import * as SecureStore from "expo-secure-store"
 import React from "react"
 import { ScrollView, StyleSheet, Text, View } from "react-native"
+import { BACKEND_URL } from "../config"
 
 export default function UesrScreen() {
+  const [user, setUser] = React.useState(null)
+  React.useEffect(() => {
+    fetchUser()
+  }, [])
+
+  const fetchUser = async () => {
+    try {
+      const u = await SecureStore.getItemAsync("aumo")
+      const res = await axios.get(BACKEND_URL + "/me", {
+        headers: {
+          Cookie: u
+        },
+        withCredentials: true
+      })
+      setUser(res.data)
+    } catch (e) {}
+  }
+
   return (
     <View style={styles.container}>
       <ScrollView
