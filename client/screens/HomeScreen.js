88665bbc4c92e6282ab1d62af6504d4b3c0228dc
Simo Aleksandrov <simo3003@me.com>
2019-10-06 03:45:17 +0300

    rest

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -2,6 +2,7 @@ import React from "react"
 import {
   Image,
   Platform,
+  TouchableWithoutFeedback,
   ScrollView,
   StyleSheet,
   Text,
@@ -29,13 +30,19 @@ export default function HomeScreen() {
             Approach phone to an aumo device, to get your digital receipt
           </Text>
         </View>
-        <Circle size={240}>
-          <Circle size={200}>
-            <View style={{ alignItems: "center" }}>
-              <AntDesign name="wifi" size={60} color="#083AA4" />
-            </View>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            console.log("rere")
+          }}
+        >
+          <Circle size={240}>
+            <Circle size={200}>
+              <View style={{ alignItems: "center" }}>
+                <AntDesign name="wifi" size={60} color="#083AA4" />
+              </View>
+            </Circle>
           </Circle>
-        </Circle>
+        </TouchableWithoutFeedback>
         <View style={styles.helpContainer}></View>
       </ScrollView>
     </View>
@@ -71,71 +78,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginHorizontal: 50
   },
-  homeScreenFilename: {
-    marginVertical: 7
-  },
-  codeHighlightText: {
-    color: "rgba(96,100,109, 0.8)"
-  },
-  codeHighlightContainer: {
-    backgroundColor: "rgba(0,0,0,0.05)",
-    borderRadius: 3,
-    paddingHorizontal: 4
-  },
   getStartedText: {
     paddingHorizontal: 18,
     fontSize: 18,
     fontWeight: "700",
     color: "#083AA4",
     textTransform: "uppercase",
-    // lineHeight: 24,
     marginTop: 50,
     textAlign: "center"
-  },
-  tabBarInfoContainer: {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: "black",
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3
-      },
-      android: {
-        elevation: 20
-      }
-    }),
-    alignItems: "center",
-    backgroundColor: "#fbfbfb",
-    paddingVertical: 20
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: "rgba(96,100,109, 1)",
-    textAlign: "center"
-  },
-  navigationFilename: {
-    marginTop: 5
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: "center"
-  },
-  helpLink: {
-    paddingVertical: 15
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: "#2e78b7"
-  },
-  inputform: {
-    paddingRight: 32,
-    paddingLeft: 32
-  },
-  emailInput: {
-    marginBottom: 10
   }
 })
