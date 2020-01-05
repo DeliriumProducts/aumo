95ced8f7b5070be361972e2f4cbfb7addce30994
Simo Aleksandrov <simo3003@me.com>
2020-01-05 23:16:43 +0200

    uh sql brooo

--- sql/aumo.sql
+++ sql/aumo.sql
@@ -1,143 +1,43 @@
--- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
---
--- Host: localhost    Database: aumo
--- ------------------------------------------------------
--- Server version	8.0.17
+CREATE TABLE users (
+  id int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  created_at timestamp NULL DEFAULT NULL,
+  updated_at timestamp NULL DEFAULT NULL,
+  deleted_at timestamp NULL DEFAULT NULL,
+  name varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  avatar varchar(255) NOT NULL,
+  points double NOT NULL,
+  UNIQUE KEY email (email)
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE TABLE products (
+  id int (10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  created_at timestamp NULL DEFAULT NULL,
+  updated_at timestamp NULL DEFAULT NULL,
+  deleted_at timestamp NULL DEFAULT NULL,
+  name varchar (255) DEFAULT NULL,
+  price double DEFAULT NULL,
+  image varchar (255) DEFAULT NULL,
+  description varchar (255) DEFAULT NULL,
+  stock int (10) unsigned DEFAULT NULL
+);
 
---
--- Table structure for table `receipts`
---
+CREATE TABLE receipts (
+  id int (10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  created_at timestamp NULL DEFAULT NULL,
+  updated_at timestamp NULL DEFAULT NULL,
+  deleted_at timestamp NULL DEFAULT NULL,
+  content varchar(255) NOT NULL,
+  user_id int(10) unsigned NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
 
-DROP TABLE IF EXISTS `receipts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `receipts` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  `content` varchar(255) DEFAULT NULL,
-  `user_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idx_receipts_deleted_at` (`deleted_at`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `receipts`
---
-
-LOCK TABLES `receipts` WRITE;
-/*!40000 ALTER TABLE `receipts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `receipts` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `shop_items`
---
-
-DROP TABLE IF EXISTS `shop_items`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `shop_items` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `price` double DEFAULT NULL,
-  `image` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `stock` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idx_shop_items_deleted_at` (`deleted_at`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `shop_items`
---
-
-LOCK TABLES `shop_items` WRITE;
-/*!40000 ALTER TABLE `shop_items` DISABLE KEYS */;
-INSERT INTO `shop_items` VALUES (1,'2019-10-05 19:43:17','2019-10-05 19:43:17',NULL,'Lidl 25% off',250,'https://www.supermarketnews.com/sites/supermarketnews.com/files/styles/article_featured_retina/public/Lidl_US_store_exterior.png?itok=ygwb_d3h','Lidl 25% off',500),(2,'2019-10-05 19:45:16','2019-10-05 19:45:16',NULL,'Billa 15% off',100,'https://www.agrobusiness.bg/images/cache/68097e541d2f6ba1cc10889632311274_w744_h500_cp.jpg','Billa 15% off',400),(3,'2019-10-05 19:46:12','2019-10-05 19:46:12',NULL,'Technopolis 30% off',150,'https://www.technopolis.bg/medias/sys_master/h78/h38/11382252896286.jpg','Technopolis 30% off',350),(4,'2019-10-05 19:47:01','2019-10-05 19:47:01',NULL,'MediaMarkt 50% off',300,'https://s9783.pcdn.co/wp-content/uploads/2017/04/MediaMarkt-Sweden-Store_small.jpg','MediaMarkt 30% off',100),(5,'2019-10-05 19:56:19','2019-10-05 19:56:19',NULL,'School Backpack',700,'http://pngimg.com/uploads/backpack/backpack_PNG6334.png','Wool.',250),(6,'2019-10-05 19:57:22','2019-10-05 19:57:22',NULL,'Toshiba MV13P3 13',3000,'https://i.ebayimg.com/images/g/WPMAAOSwQ3RcagQX/s-l640.jpg','Yeet.',250);
-/*!40000 ALTER TABLE `shop_items` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user_shop_item`
---
-
-DROP TABLE IF EXISTS `user_shop_item`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user_shop_item` (
-  `user_id` int(10) unsigned NOT NULL,
-  `shop_item_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`user_id`,`shop_item_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_shop_item`
---
-
-LOCK TABLES `user_shop_item` WRITE;
-/*!40000 ALTER TABLE `user_shop_item` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user_shop_item` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `avatar` varchar(255) DEFAULT NULL,
-  `points` double NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`),
-  KEY `idx_users_deleted_at` (`deleted_at`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'2019-10-05 19:47:23','2019-10-05 19:47:23',NULL,'Simo Aleksandrov','simo3003@me.com','$2a$12$4rY.uV7RL1QUtSDW2.UEU.XaxBbDAMljKKovX3PmInlHSGuU8qPCG','https://i.imgur.com/z8JO3A4.png',5000),(2,'2019-10-05 19:58:45','2019-10-05 19:58:45',NULL,'Lyubo Lyubchev','lyubo_2317@abv.bg','$2a$12$F4kIsQk89mRDLHEEZ25b0OC2x91.BV8O3RcAXHgkIbgZXOJY44rrC','https://i.imgur.com/4Ws6pd9.png',5000);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-10-06  1:59:03
+CREATE TABLE orders (
+    userId int (10) unsigned NOT NULL,
+    productId int (10) unsigned NOT NULL,
+    quantity int (11) NOT NULL,
+    PRIMARY KEY (userId, productId),
+    FOREIGN KEY (productId) REFERENCES products(id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE ON UPDATE NO ACTION
+);
