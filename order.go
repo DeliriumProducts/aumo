af5c4e81f0027bae7ce9b89368c77b8ddf685d6e
Simo Aleksandrov <simo3003@me.com>
2020-01-07 19:22:02 +0200

    inline

--- order.go
+++ order.go
@@ -1,9 +1,10 @@
 package aumo
 
 type Order struct {
-	ID        uint `json:"id" db:"id,omitempty"`
-	UserID    uint `json:"userID" db:"user_id"`
-	ProductID uint `json:"productID" db:"product_id"`
+	ID        uint    `json:"id" db:"id,omitempty"`
+	UserID    uint    `json:"userID" db:"user_id"`
+	ProductID uint    `json:"productID" db:"product_id"`
+	Product   Product `json:"product" db:"-"`
 }
 
 // OrderService contains all `Order`
