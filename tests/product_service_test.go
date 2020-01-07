3baed51213a85a750c4ed25e75437e91e3db2190
Simo Aleksandrov <simo3003@me.com>
2020-01-07 18:35:17 +0200

    tests

--- tests/product_service_test.go
+++ tests/product_service_test.go
@@ -28,7 +28,7 @@ func TestProductService(t *testing.T) {
 
 		pd := aumo.NewProduct("TV", 500, "image.com", "ok", 5)
 		err := ps.Create(pd)
-		assert.Nil(t, err, "didn't return an error")
+		assert.Nil(t, err, "shouldn't return an error")
 
 		pm := aumo.Product{}
 		sess.Collection("products").Find("id", pd.ID).One(&pm)
@@ -40,11 +40,34 @@ func TestProductService(t *testing.T) {
 
 		pd := aumo.NewProduct("Laptop", 100, "image.com", "it's a good laptop", 5)
 		err := ps.Create(pd)
-		assert.Nil(t, err, "didn't return an error")
+		assert.Nil(t, err, "shouldn't return an error")
 
 		pm, err := ps.Product(pd.ID)
-		assert.Nil(t, err, "didn't return an error")
+		assert.Nil(t, err, "shouldn't return an error")
 		assert.Equal(t, *pd, *pm)
+	})
+
+	t.Run("get_products", func(t *testing.T) {
+		defer TidyDB(sess)
+
+		pds := []*aumo.Product{
+			aumo.NewProduct("Phone", 100, "image.com", "it's a good phone", 5),
+			aumo.NewProduct("ok", 100, "image.com", "it's a good phone", 5),
+			aumo.NewProduct("TV", 100, "image.tv", "it's a good tv", 6),
+		}
+
+		for _, pd := range pds {
+			err := ps.Create(pd)
+			assert.Nil(t, err, "shouldn't return an error")
+		}
+
+		pms, err := ps.Products()
+		assert.Nil(t, err, "it shouldn't return an error")
+		assert.Equal(t, len(pms), len(pds), "it should have equal length")
+
+		for i := 0; i < len(pms); i++ {
+			assert.Equal(t, *pds[i], pms[i], "it should be equal")
+		}
 	})
 
 	t.Run("delete_product", func(t *testing.T) {
@@ -52,10 +75,10 @@ func TestProductService(t *testing.T) {
 
 		pd := aumo.NewProduct("Phone", 99, "image.com", "it's a good phone", 2)
 		err := ps.Create(pd)
-		assert.Nil(t, err, "didn't return an error")
+		assert.Nil(t, err, "shouldn't return an error")
 
 		err = ps.Delete(pd.ID)
-		assert.Nil(t, err, "didn't return an error")
+		assert.Nil(t, err, "shouldn't return an error")
 
 		pm := aumo.Product{}
 		err = sess.Collection("products").Find("id", pd.ID).One(&pm)
