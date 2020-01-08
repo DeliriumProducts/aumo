79c9f1ae738667dff7ac25ea86d92a6a29fb06f0
Simo Aleksandrov <simo3003@me.com>
2020-01-08 11:33:42 +0200

    add dummy data

--- data.sql
+++ data.sql
@@ -1,3 +1,16 @@
-INSERT INTO `users` VALUES (1,'Simo','simo3003@me.com','123','github.com/fr3fou.png',5000),(2,'Pesho','pesho@me.com','1234','github.com/pesho.png',5000);
-INSERT INTO `products` VALUES (1,'TV',500,'image',"it's decent",5),(2,'PC',500,'PC ok',"it's bad",5);
-INSERT INTO `receipts` VALUES (1,"Paconi: 250 leva",1),(2,"Lidl: 500 leva",2);
+INSERT INTO `users` VALUES 
+    (1,'Simo','simo3003@me.com','123','github.com/fr3fou.png',5000),
+    (2,'Pesho','pesho@me.com','1234','github.com/pesho.png',5000),
+    (3,'Lyubo','lyubo@me.com','1234234','github.com/impzero.png',5000);
+INSERT INTO `products` VALUES 
+    (1,'TV',500,'image',"it's decent",5),
+    (2,'PC',500,'PC ok',"it's bad",5),
+    (3,'Phone',500,'iphone ok',"it's meh",5);
+INSERT INTO `receipts` VALUES 
+    (1,"Paconi: 250 leva",1),
+    (2,"Lidl: 500 leva",2);
+INSERT INTO `orders` VALUES 
+    (1, 1, 2),
+    (2, 1, 1),
+    (3, 1, 3),
+    (4, 2, 1);
