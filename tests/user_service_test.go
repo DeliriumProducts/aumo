b873bf2cc6498256f010c6ef8240da921570c34b
Simo Aleksandrov <simo3003@me.com>
2020-01-11 14:58:20 +0200

    test get user

--- tests/user_service_test.go
+++ tests/user_service_test.go
@@ -40,4 +40,29 @@ func TestUserService(t *testing.T) {
 		um.Orders = []aumo.Order{}
 		assert.Equal(t, *u, *um)
 	})
+
+	t.Run("get_user", func(t *testing.T) {
+		defer TidyDB(sess)
+
+		u, err := aumo.NewUser("George", "go@sho.com", "1234", "asdf")
+		assert.Nil(t, err, "shouldn't return an error")
+		err = us.Create(u)
+		assert.Nil(t, err, "shouldn't return an error")
+
+		us, err := us.User(u.ID, false)
+		assert.Nil(t, err, "shouldn't return an error")
+		us.Receipts = []aumo.Receipt{}
+		us.Orders = []aumo.Order{}
+		assert.Equal(t, *u, *us, "should be equal")
+	})
+
+	t.Run("claim_receipt", func(t *testing.T) {
+		defer TidyDB(sess)
+
+		u, err := aumo.NewUser("Adrian", "adrian@pesho.com", "123456", "ok")
+		assert.Nil(t, err, "shouldn't return an error")
+		err = us.Create(u)
+		assert.Nil(t, err, "shouldn't return an error")
+
+	})
 }
