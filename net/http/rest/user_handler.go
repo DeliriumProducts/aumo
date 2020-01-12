57425bbb72219ea0a61d25735748bc29bd389212
Simo Aleksandrov <simo3003@me.com>
2020-01-12 20:44:37 +0200

    fix

--- net/http/rest/user_handler.go
+++ net/http/rest/user_handler.go
@@ -32,7 +32,7 @@ func (rest *Rest) RegisterHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = rest.UserService.Create(user)
+	err = rest.us.Create(user)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
