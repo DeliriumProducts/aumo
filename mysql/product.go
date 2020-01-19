c43a098de8c3f04fa16b617003dad6d29b49230f
Simo Aleksandrov <simo3003@me.com>
2020-01-19 16:53:48 +0200

    product supports tx

--- mysql/product.go
+++ mysql/product.go
@@ -24,38 +24,128 @@ func (p *productStore) DB() sqlbuilder.Database {
 }
 
 func (p *productStore) FindByID(tx aumo.Tx, id uint) (*aumo.Product, error) {
-	if tx == nil {
-		tx = p.db
-	}
+	var err error
 	product := &aumo.Product{}
+
+	if tx == nil {
+		tx, err = p.db.NewTx(nil)
+
+		if err != nil {
+			return nil, err
+		}
+
+		defer func() {
+			if p := recover(); p != nil {
+				tx.Rollback()
+				panic(p)
+			} else if err != nil {
+				tx.Rollback()
+			} else {
+				err = tx.Commit()
+			}
+		}()
+	}
+
 	return product, tx.Collection(ProductTable).Find("id", id).One(product)
 }
 
 func (p *productStore) FindAll(tx aumo.Tx) ([]aumo.Product, error) {
-	if tx == nil {
-		tx = p.db
-	}
+	var err error
 	products := []aumo.Product{}
+
+	if tx == nil {
+		tx, err = p.db.NewTx(nil)
+
+		if err != nil {
+			return nil, err
+		}
+
+		defer func() {
+			if p := recover(); p != nil {
+				tx.Rollback()
+				panic(p)
+			} else if err != nil {
+				tx.Rollback()
+			} else {
+				err = tx.Commit()
+			}
+		}()
+	}
+
 	return products, tx.Collection(ProductTable).Find().All(&products)
 }
 
 func (p *productStore) Save(tx aumo.Tx, pd *aumo.Product) error {
+	var err error
+
 	if tx == nil {
-		tx = p.db
+		tx, err = p.db.NewTx(nil)
+
+		if err != nil {
+			return err
+		}
+
+		defer func() {
+			if p := recover(); p != nil {
+				tx.Rollback()
+				panic(p)
+			} else if err != nil {
+				tx.Rollback()
+			} else {
+				err = tx.Commit()
+			}
+		}()
 	}
+
 	return tx.Collection(ProductTable).InsertReturning(pd)
 }
 
 func (p *productStore) Update(tx aumo.Tx, id uint, pd *aumo.Product) error {
+	var err error
+
 	if tx == nil {
-		tx = p.db
+		tx, err = p.db.NewTx(nil)
+
+		if err != nil {
+			return err
+		}
+
+		defer func() {
+			if p := recover(); p != nil {
+				tx.Rollback()
+				panic(p)
+			} else if err != nil {
+				tx.Rollback()
+			} else {
+				err = tx.Commit()
+			}
+		}()
 	}
+
 	return tx.Collection(ProductTable).Find("id", id).Update(pd)
 }
 
 func (p *productStore) Delete(tx aumo.Tx, id uint) error {
+	var err error
+
 	if tx == nil {
-		tx = p.db
+		tx, err = p.db.NewTx(nil)
+
+		if err != nil {
+			return err
+		}
+
+		defer func() {
+			if p := recover(); p != nil {
+				tx.Rollback()
+				panic(p)
+			} else if err != nil {
+				tx.Rollback()
+			} else {
+				err = tx.Commit()
+			}
+		}()
 	}
+
 	return tx.Collection(ProductTable).Find("id", id).Delete()
 }
