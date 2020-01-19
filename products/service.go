163d32102d08d52bc86621dc91eb5b555a753fc5
Simo Aleksandrov <simo3003@me.com>
2020-01-19 03:22:01 +0200

    fix product service

--- products/service.go
+++ products/service.go
@@ -14,21 +14,21 @@ func New(store aumo.ProductStore) aumo.ProductService {
 }
 
 func (ps *service) Product(id uint) (*aumo.Product, error) {
-	return ps.store.FindByID(id)
+	return ps.store.FindByID(nil, id)
 }
 
 func (ps *service) Products() ([]aumo.Product, error) {
-	return ps.store.FindAll()
+	return ps.store.FindAll(nil)
 }
 
 func (ps *service) Create(p *aumo.Product) error {
-	return ps.store.Save(p)
+	return ps.store.Save(nil, p)
 }
 
 func (ps *service) Update(id uint, p *aumo.Product) error {
-	return ps.store.Update(id, p)
+	return ps.store.Update(nil, id, p)
 }
 
 func (ps *service) Delete(id uint) error {
-	return ps.store.Delete(id)
+	return ps.store.Delete(nil, id)
 }
