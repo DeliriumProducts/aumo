640f14c56a61395928eabba8c90c5df7c3fe6179
Lyubo Lyubchev <lyubo_2317@abv.bg>
2020-01-19 22:28:37 +0200

    add endpoints for product

--- net/http/rest/products_handlers.go
+++ net/http/rest/products_handlers.go
@@ -24,6 +24,7 @@ func (rest *Rest) productHandlerCreate(w http.ResponseWriter, r *http.Request) {
 		Description string  `form:"description" validate:"required"`
 		Stock       uint    `form:"stock" validate:"required,numeric"`
 	}
+
 	var npf request
 	if ok := rest.Form(w, r, &npf); !ok {
 		return
@@ -34,6 +35,45 @@ func (rest *Rest) productHandlerCreate(w http.ResponseWriter, r *http.Request) {
 	err := rest.productService.Create(product)
 	if err != nil {
 		rest.JSONError(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	rest.JSON(w, product, http.StatusOK)
+}
+
+func (rest *Rest) productHandlerGet(w http.ResponseWriter, r *http.Request) {
+	productID := rest.ParamNumber(w, r, "id")
+
+	order, err := rest.productService.Product(productID)
+	if err != nil {
+		rest.JSONError(w, err, http.StatusNotFound)
+		return
+	}
+
+	rest.JSON(w, order, http.StatusOK)
+}
+
+func (rest *Rest) productHandlerEdit(w http.ResponseWriter, r *htpp.Request) {
+	productID := rest.ParamNumber(w, r, "id")
+
+	type request struct {
+		Name        string  `form:"name" validate:"required"`
+		Image       string  `form:"image" validate:"required,url"`
+		Price       float64 `form:"price" validate:"required,numeric"`
+		Description string  `form:"description" validate:"required"`
+		Stock       uint    `form:"stock" validate:"required,numeric"`
+	}
+
+	var npf request
+	if ok := rest.Form(w, r, &npf); !ok {
+		return
+	}
+
+	product := aumo.NewProduct(npf.Name, npf.Price, npf.Image, npf.Description, npf.Stock)
+
+	err := rest.productService.Update(productID, product)
+	if err != nil {
+		rest.JSONError(w, err, http.StatusNotFound)
 		return
 	}
 
