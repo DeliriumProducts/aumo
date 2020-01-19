de2d4b89334cff54b47bbac208b531abcb631a14
Simo Aleksandrov <simo3003@me.com>
2020-01-19 16:55:41 +0200

    receipt supports tx

--- mysql/receipt.go
+++ mysql/receipt.go
@@ -24,38 +24,128 @@ func (r *receiptStore) DB() sqlbuilder.Database {
 }
 
 func (r *receiptStore) FindByID(tx aumo.Tx, id uint) (*aumo.Receipt, error) {
-	if tx == nil {
-		tx = r.db
-	}
+	var err error
 	receipt := &aumo.Receipt{}
+
+	if tx == nil {
+		tx, err = r.db.NewTx(nil)
+
+		if err != nil {
+			return nil, err
+		}
+
+		defer func() {
+			if p := recover(); p != nil {
+				tx.Rollback()
+				panic(p)
+			} else if err != nil {
+				tx.Rollback()
+			} else {
+				err = tx.Commit()
+			}
+		}()
+	}
+
 	return receipt, tx.Collection(ReceiptTable).Find("receipt_id", id).One(receipt)
 }
 
 func (r *receiptStore) FindAll(tx aumo.Tx) ([]aumo.Receipt, error) {
-	if tx == nil {
-		tx = r.db
-	}
+	var err error
 	receipts := []aumo.Receipt{}
+
+	if tx == nil {
+		tx, err = r.db.NewTx(nil)
+
+		if err != nil {
+			return nil, err
+		}
+
+		defer func() {
+			if p := recover(); p != nil {
+				tx.Rollback()
+				panic(p)
+			} else if err != nil {
+				tx.Rollback()
+			} else {
+				err = tx.Commit()
+			}
+		}()
+	}
+
 	return receipts, tx.Collection(ReceiptTable).Find().All(&receipts)
 }
 
 func (r *receiptStore) Save(tx aumo.Tx, rs *aumo.Receipt) error {
+	var err error
+
 	if tx == nil {
-		tx = r.db
+		tx, err = r.db.NewTx(nil)
+
+		if err != nil {
+			return err
+		}
+
+		defer func() {
+			if p := recover(); p != nil {
+				tx.Rollback()
+				panic(p)
+			} else if err != nil {
+				tx.Rollback()
+			} else {
+				err = tx.Commit()
+			}
+		}()
 	}
+
 	return tx.Collection(ReceiptTable).InsertReturning(rs)
 }
 
 func (r *receiptStore) Update(tx aumo.Tx, id uint, rr *aumo.Receipt) error {
+	var err error
+
 	if tx == nil {
-		tx = r.db
+		tx, err = r.db.NewTx(nil)
+
+		if err != nil {
+			return err
+		}
+
+		defer func() {
+			if p := recover(); p != nil {
+				tx.Rollback()
+				panic(p)
+			} else if err != nil {
+				tx.Rollback()
+			} else {
+				err = tx.Commit()
+			}
+		}()
 	}
+
 	return tx.Collection(ReceiptTable).Find("receipt_id", id).Update(rr)
 }
 
 func (r *receiptStore) Delete(tx aumo.Tx, id uint) error {
+	var err error
+
 	if tx == nil {
-		tx = r.db
+		tx, err = r.db.NewTx(nil)
+
+		if err != nil {
+			return err
+		}
+
+		defer func() {
+			if p := recover(); p != nil {
+				tx.Rollback()
+				panic(p)
+			} else if err != nil {
+				tx.Rollback()
+			} else {
+				err = tx.Commit()
+			}
+		}()
 	}
+
 	return r.db.Collection(ReceiptTable).Find("receipt_id", id).Delete()
 }
