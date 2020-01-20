bc23a694546316905c86db6a12a42c456643a032
Simo Aleksandrov <simo3003@me.com>
2020-01-20 23:49:29 +0200

    product api

--- client/api/product.js
+++ client/api/product.js
@@ -7,18 +7,6 @@ export class ProductAPI {
   }
 
   static async getAll() {
-    return await axios.post(`${BACKEND_URL}/login`, creds, this.opts)
-  }
-
-  static async register(creds) {
-    return await axios.post(`${BACKEND_URL}/register`, creds)
-  }
-
-  static async me() {
-    return await axios.get(`${BACKEND_URL}/me`, this.opts)
-  }
-
-  static async logout() {
-    return await axios.get(`${BACKEND_URL}/logout`, this.opts)
+    return await axios.get(`${BACKEND_URL}/products`, this.opts)
   }
 }
