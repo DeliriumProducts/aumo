44d52b143542af5ac42a6da592f732a6bf7f0d61
Lyubo Lyubchev <lyubo_2317@abv.bg>
2020-01-20 22:01:08 +0200

    claim receipt

--- net/http/rest/receipt_handler.go
+++ net/http/rest/receipt_handler.go
@@ -3,10 +3,10 @@ package rest
 import (
 	"net/http"
 
-	"github.com/deliriumproducts/aumo"
+	"github.com/deliriumproducts/aumo/auth"
 )
 
-func (rest *Rest) receiptsCreate(w http.ResponseWriter, r *http.Request) {
+func (rest *Rest) receiptCreate(w http.ResponseWriter, r *http.Request) {
 	type request struct {
 		Content string `form:"content" validate:"required"`
 	}
@@ -16,9 +16,27 @@ func (rest *Rest) receiptsCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	receipt := aumo.NewReceipt(re.Content)
+	receipt := auth.NewReceipt(re.Content)
 
 	err := rest.receiptService.Create(receipt)
+	if err != nil {
+		rest.JSONError(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	rest.JSON(w, receipt, http.StatusOK)
+}
+
+func (rest *Rest) receiptClaim(w http.ResponseWriter, r *http.Request) {
+	rID := rest.ParamNumber(w, r, "id")
+
+	user, err := auth.CurrentUser(r.Context())
+	if err != nil {
+		rest.JSONError(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	receipt, err := rest.receiptService.ClaimReceipt(user.ID, rID)
 	if err != nil {
 		rest.JSONError(w, err, http.StatusInternalServerError)
 		return
