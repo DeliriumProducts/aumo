68e88d9cfccdb86856c98491f11767c41ad3fca3
Lyubo Lyubchev <lyubo_2317@abv.bg>
2020-01-23 00:50:42 +0200

    remove static opts

--- js-api/auth.js
+++ js-api/auth.js
@@ -1,29 +1,29 @@
-const axios = require("axios")
+const axios = require("axios");
 
 class AuthAPI {
-  static opts = {
+  opts = {
     withCredentials: true
-  }
+  };
 
   constructor(backend) {
-    this.backend = backend
+    this.backend = backend;
   }
 
   async login(creds) {
-    return await axios.post(`${this.backend}/login`, creds, this.opts)
+    return await axios.post(`${this.backend}/login`, creds, this.opts);
   }
 
   async register(creds) {
-    return await axios.post(`${this.backend}/register`, creds)
+    return await axios.post(`${this.backend}/register`, creds);
   }
 
   async logout() {
-    return await axios.get(`${this.backend}/logout`, this.opts)
+    return await axios.get(`${this.backend}/logout`, this.opts);
   }
 
   async me() {
-    return await axios.get(`${this.backend}/me`, this.opts)
+    return await axios.get(`${this.backend}/me`, this.opts);
   }
 }
 
-export default AuthAPI
+export default AuthAPI;
