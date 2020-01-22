4cebfca9610e7da0479b3db51072979bf8c405d4
Simo Aleksandrov <simo3003@me.com>
2020-01-22 08:45:46 +0200

    subpoints

--- user.go
+++ user.go
@@ -108,6 +108,7 @@ type UserService interface {
 	Update(id uint, u *User) error
 	EditRole(id uint, role Role) error
 	AddPoints(id uint, points float64) error
+	SubPoints(id uint, points float64) error
 	Delete(id uint) error
 }
 
