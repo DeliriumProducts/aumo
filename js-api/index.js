141ee4a8e1d6f3f084a4dd9b277b4fca85df431b
Lyubo Lyubchev <lyubo_2317@abv.bg>
2020-01-23 10:09:32 +0200

    ok

--- js-api/index.js
+++ js-api/index.js
@@ -1,13 +1,13 @@
-const AuthAPI = require("./auth.js")
-const UserAPI = require("./user.js")
-const ProductAPI = require("./product.js")
-const ReceiptAPI = require("./receipt.js")
-const OrderAPI = require("./order.js")
+const AuthAPI = require('./auth.js');
+const UserAPI = require('./user.js');
+const ProductAPI = require('./product.js');
+const ReceiptAPI = require('./receipt.js');
+const OrderAPI = require('./order.js');
 
-modules.export = {
+module.exports = {
   AuthAPI,
   UserAPI,
   ProductAPI,
   ReceiptAPI,
   OrderAPI
-}
+};
