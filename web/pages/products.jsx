85244c56d2180befb26264d6d1b06df02d4f66d2
Lyubo Lyubchev <lyubo_2317@abv.bg>
2020-01-24 01:26:14 +0200

    resolve import

--- web/pages/products.jsx
+++ web/pages/products.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head"
-import { withAuth } from "../hocs/withAuth"
+import withAuth from "../hocs/withAuth"
+import styled from "styled-components"
 
 export const Products = () => (
   <>
@@ -7,8 +8,18 @@ export const Products = () => (
       <title>Aumo</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    Products
+    <Container>Products</Container>
   </>
 )
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media only screen and (max-width: 600px) {
+    flex-direction: column;
+  }
+`
 
 export default withAuth(Products)
