e707b828deef431488cb592e173cb4d680fd921a
Simo Aleksandrov <simo3003@me.com>
2020-01-24 16:15:56 +0200

    fix data

--- data.sql
+++ data.sql
@@ -1,7 +1,7 @@
 INSERT INTO `users` VALUES 
-    (1,'Simo','simo3003@me.com','123','github.com/fr3fou.png', 5000, "Admin"),
-    (2,'Pesho','pesho@me.com','1234','github.com/pesho.png', 5000, "Customer"),
-    (3,'Lyubo','lyubo@me.com','1234234','github.com/impzero.png', 5000, "Admin");
+    (1,'Simo','simo3003@me.com','123','https://github.com/fr3fou.png', 5000, "Admin"),
+    (2,'Pesho','pesho@me.com','1234','https://github.com/pesho.png', 5000, "Customer"),
+    (3,'Lyubo','lyubo@me.com','1234234','https://github.com/impzero.png', 5000, "Admin");
 INSERT INTO `products` VALUES 
     (1,'Akai 32-inch HD LED LCD',500,'https://azcd.harveynorman.com.au/media/catalog/product/cache/21/image/992x558/9df78eab33525d08d6e5fb8d27136e95/a/k/ak3219nf.jpg',"Enjoy watching your favourite movies and shows in stunning HD quality with the Akai 32-inch HD LED LCD Smart TV.",5),
     (2,'CYBERPOWERPC Gamer Master Gaming PC',500,'https://images-na.ssl-images-amazon.com/images/I/812kz16Md0L._SX466_.jpg',"Cyber PowerPC Gamer Master series is a line of gaming PCs powered by AMD's newest Ryzen CPU and accompanying AM4 architecture.",5),
