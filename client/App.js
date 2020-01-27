014aaad97e97f9eb0521670ee883de8c151f783c
Simo Aleksandrov <simo3003@me.com>
2020-01-28 00:32:06 +0200

    opensans

--- client/App.js
+++ client/App.js
@@ -3,22 +3,39 @@ import {
   ApplicationProvider,
   Layout,
   Text,
-  Button
+  IconRegistry,
+  Button,
+  Icon
 } from "@ui-kitten/components"
 import { mapping } from "@eva-design/eva"
+import { EvaIconsPack } from "@ui-kitten/eva-icons"
 import theme from "./theme"
+import customM from "./mapping"
+
+export const FacebookIcon = style => <Icon name="facebook" {...style} />
+
+export const LoginButton = () => (
+  <Button icon={FacebookIcon}>Login with Facebook</Button>
+)
 
 const HomeScreen = () => (
   <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <Text category="h1">HOME</Text>
-    <Button>hiii!</Button>
+    <Text category="h1">Home</Text>
+    <LoginButton />
   </Layout>
 )
 
 const App = () => (
-  <ApplicationProvider mapping={mapping} theme={theme}>
-    <HomeScreen />
-  </ApplicationProvider>
+  <>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider
+      mapping={mapping}
+      theme={theme}
+      customMapping={customM}
+    >
+      <HomeScreen />
+    </ApplicationProvider>
+  </>
 )
 
 export default App
