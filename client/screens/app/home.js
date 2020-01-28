da7d79b090d30cc92f34a13b27cc3682a916b728
Simo Aleksandrov <simo3003@me.com>
2020-01-29 00:39:06 +0200

    uh text not view

--- client/screens/app/home.js
+++ client/screens/app/home.js
@@ -1,6 +1,6 @@
-import { View } from "react-native"
+import { Text } from "react-native"
 import React from "react"
 
 export default () => {
-  return <View>Home!</View>
+  return <Text>Home!</Text>
 }
