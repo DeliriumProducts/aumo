a3de07981415be55af88e54e44f3e886a337724b
Simo Aleksandrov <simo3003@me.com>
2020-01-29 16:20:30 +0200

    all types

--- js-api/src/lib/aumo.ts
+++ js-api/src/lib/aumo.ts
@@ -6,3 +6,29 @@ export interface Product {
   description: string;
   stock: number;
 }
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  avatar: string;
+  points: number;
+  role: Role;
+  orders: Order[];
+  receipts: Receipt[];
+}
+
+export type Role = 'Admin' | 'Customer';
+
+export interface Order {
+  order_id: number;
+  user_id: number;
+  product_id: number;
+  product: Product;
+}
+
+export interface Receipt {
+  receipt_id: number;
+  content: string;
+}
