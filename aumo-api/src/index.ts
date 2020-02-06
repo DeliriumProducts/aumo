db591f2e1fd8f43fcdcd859f64b43d1ac0a148f9
Simo Aleksandrov <simo3003@me.com>
2020-02-06 04:27:09 +0200

    export all things

--- aumo-api/src/index.ts
+++ aumo-api/src/index.ts
@@ -1,3 +1,7 @@
 export * from './lib/aumo';
 export * from './lib/auth';
 export * from './lib/config';
+export * from './lib/order';
+export * from './lib/product';
+export * from './lib/receipt';
+export * from './lib/user';
