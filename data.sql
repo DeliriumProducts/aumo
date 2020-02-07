e63822c89a146d7ac02075a9d7ad8a4998715975
Simo Aleksandrov <simo3003@me.com>
2020-02-07 22:49:41 +0200

    data sql with bcrypt

--- data.sql
+++ data.sql
@@ -1,7 +1,7 @@
-INSERT INTO `users` VALUES 
-    ('24a2068c-a43d-43ae-b967-858953686189','Simo','simo3003@me.com','123','https://github.com/fr3fou.png', 5000, "Admin", 1),
-    ('19274de1-ac2b-4dd9-adee-20f4ab69e920','Pesho','pesho@me.com','1234','https://github.com/pesho.png', 5000, "Customer", 1),
-    ('1cd005fe-213f-4c96-9966-49306b00ca90','Lyubo','lyubo@me.com','1234234','https://github.com/impzero.png', 5000, "Admin", 1);
+INSERT INTO `users` VALUES  -- all user's passwords are 123456
+    ('24a2068c-a43d-43ae-b967-858953686189','Simo','simo3003@me.com','$2y$12$OzLPsg4GRS2ALckMY7QRIe.49zi2HnwL7kHYCbuMsR8Y4te3G.N9e','https://github.com/fr3fou.png', 5000, "Admin", 1),
+    ('19274de1-ac2b-4dd9-adee-20f4ab69e920','Pesho','pesho@me.com','$2y$12$YRKTtih7Cy1kN9ZayAW9Fe.AOfZypr40io8byn4ghfJWWH.aZf2ca','https://github.com/pesho.png', 5000, "Customer", 1),
+    ('1cd005fe-213f-4c96-9966-49306b00ca90','Lyubo','lyubo@me.com','$2y$12$w5Bs/vKa0D1uEKlo6AH8QexQujTjtSRIecBEwkSyo/nRzkR4KOdHO','https://github.com/impzero.png', 5000, "Admin", 1);
 INSERT INTO `products` VALUES 
     (1,'Akai 32-inch HD LED LCD',500,'https://azcd.harveynorman.com.au/media/catalog/product/cache/21/image/992x558/9df78eab33525d08d6e5fb8d27136e95/a/k/ak3219nf.jpg',"Enjoy watching your favourite movies and shows in stunning HD quality with the Akai 32-inch HD LED LCD Smart TV.",5),
     (2,'CYBERPOWERPC Gamer Master Gaming PC',500,'https://images-na.ssl-images-amazon.com/images/I/812kz16Md0L._SX466_.jpg',"Cyber PowerPC Gamer Master series is a line of gaming PCs powered by AMD's newest Ryzen CPU and accompanying AM4 architecture.",5),
