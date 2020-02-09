7d90d757159a394eb06ff85dd34f09a6c0847c12
Simo Aleksandrov <simo3003@me.com>
2020-02-09 22:02:28 +0200

    fmt test lol

--- client/__tests__/App.js
+++ client/__tests__/App.js
@@ -1,10 +1,9 @@
-import 'react-native';
-import React from 'react';
-import App from '../App';
-
+import React from "react"
+import "react-native"
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer from "react-test-renderer"
+import App from "../App"
 
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+it("renders correctly", () => {
+  renderer.create(<App />)
+})
