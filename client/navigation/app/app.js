d67d567fb6b973d948b3c6d3569dea55fa17a996
Simo Aleksandrov <simo3003@me.com>
2020-02-10 23:51:20 +0200

    show user's avatar in nav

--- client/navigation/app/app.js
+++ client/navigation/app/app.js
@@ -1,7 +1,8 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
-import { Icon } from "@ui-kitten/components"
+import { Avatar, Icon } from "@ui-kitten/components"
 import React from "react"
 import { TabBar } from "react-native-animated-nav-tab-bar"
+import { Context } from "../../context/context"
 import Routes from "../routes"
 import HomeStack from "./home"
 import ShopStack from "./shop"
@@ -9,62 +10,61 @@ import UserStack from "./user"
 
 const Tab = createBottomTabNavigator()
 
-export default () => (
-  <Tab.Navigator
-    initialRouteName={Routes.Home}
-    tabBarOptions={{
-      activeTintColor: "#083aa4",
-      inactiveTintColor: "#222222"
-    }}
-    tabBar={props => (
-      <TabBar
-        activeColors={"#083aa4"}
-        activeTabBackgrounds={"#cae0fa"}
-        {...props}
+export default () => {
+  const ctx = React.useContext(Context)
+  return (
+    <Tab.Navigator
+      initialRouteName={Routes.Home}
+      tabBarOptions={{
+        activeTintColor: "#083aa4",
+        inactiveTintColor: "#222222"
+      }}
+      tabBar={props => (
+        <TabBar
+          activeColors={"#083aa4"}
+          activeTabBackgrounds={"#cae0fa"}
+          {...props}
+        />
+      )}
+    >
+      <Tab.Screen
+        name={Routes.Home}
+        component={HomeStack}
+        options={{
+          tabBarIcon: ({ focused, color }) => (
+            <Icon
+              name="home-outline"
+              fill={focused ? color : "#222222"}
+              width={24}
+              height={24}
+            />
+          )
+        }}
       />
-    )}
-  >
-    <Tab.Screen
-      name={Routes.Home}
-      component={HomeStack}
-      options={{
-        tabBarIcon: ({ focused, tintColor }) => (
-          <Icon
-            name="home-outline"
-            fill={focused ? tintColor : "#222222"}
-            width={24}
-            height={24}
-          />
-        )
-      }}
-    />
-    <Tab.Screen
-      name={Routes.Shop}
-      component={ShopStack}
-      options={{
-        tabBarIcon: ({ focused, tintColor }) => (
-          <Icon
-            name="shopping-cart-outline"
-            fill={focused ? tintColor : "#222222"}
-            width={24}
-            height={24}
-          />
-        )
-      }}
-    />
-    <Tab.Screen
-      name={Routes.User}
-      component={UserStack}
-      options={{
-        tabBarIcon: ({ focused, tintColor }) => (
-          <Icon
-            name="person-outline"
-            fill={focused ? tintColor : "#222222"}
-            width={24}
-            height={24}
-          />
-        )
-      }}
-    />
-  </Tab.Navigator>
-)
+      <Tab.Screen
+        name={Routes.Shop}
+        component={ShopStack}
+        options={{
+          tabBarIcon: ({ focused, color }) => (
+            <Icon
+              name="shopping-cart-outline"
+              fill={focused ? color : "#222222"}
+              width={24}
+              height={24}
+            />
+          )
+        }}
+      />
+      <Tab.Screen
+        name={Routes.User}
+        component={UserStack}
+        options={{
+          tabBarLabel: "My Profile",
+          tabBarIcon: ({ focused, color }) => (
+            <Avatar source={{ uri: ctx?.state?.user.avatar }} />
+          )
+        }}
+      />
+    </Tab.Navigator>
+  )
+}
