b16770ae0899b5ce826e8fa371f407c38801dd5d
Simo Aleksandrov <simo3003@me.com>
2020-02-11 00:49:19 +0200

    loading on logout

--- client/screens/app/user.js
+++ client/screens/app/user.js
@@ -1,18 +1,41 @@
+import { Button, Spinner } from "@ui-kitten/components"
 import aumo from "aumo"
 import React from "react"
-import { Button } from "react-native"
+import { View } from "react-native"
 import { Context } from "../../context/context"
 import { actions } from "../../context/providers/provider"
 
 export default () => {
   const ctx = React.useContext(Context)
+  const [loading, setLoading] = React.useState(false)
   return (
-    <Button
-      title="Logout!"
-      onPress={async () => {
-        await aumo.auth.logout()
-        ctx.dispatch({ type: actions.SET_USER, payload: null })
-      }}
-    ></Button>
+    <>
+      <Button
+        disabled={loading}
+        size="large"
+        appearance="ghost"
+        onPress={async () => {
+          try {
+            setLoading(true)
+            await aumo.auth.logout()
+            ctx.dispatch({ type: actions.SET_USER, payload: null })
+          } catch (error) {
+          } finally {
+            setLoading(false)
+          }
+        }}
+      >
+        Logout!
+      </Button>
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%"
+        }}
+      >
+        {loading && <Spinner size="giant" />}
+      </View>
+    </>
   )
 }
