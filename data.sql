5ba75fecb6ea5d1d611687d05abcf76e0f68cd4e
Lyubo Lyubchev <lyubo_2317@abv.bg>
2020-02-12 12:37:23 +0200

    fix schema and data.sql

--- data.sql
+++ data.sql
@@ -2,6 +2,12 @@ INSERT INTO `users` VALUES  -- all user's passwords are 123456
     ('24a2068c-a43d-43ae-b967-858953686189','Simo','simo3003@me.com','$2y$12$OzLPsg4GRS2ALckMY7QRIe.49zi2HnwL7kHYCbuMsR8Y4te3G.N9e','https://github.com/fr3fou.png', 5000, "Admin", 1),
     ('19274de1-ac2b-4dd9-adee-20f4ab69e920','Pesho','pesho@me.com','$2y$12$YRKTtih7Cy1kN9ZayAW9Fe.AOfZypr40io8byn4ghfJWWH.aZf2ca','https://github.com/pesho.png', 5000, "Customer", 1),
     ('1cd005fe-213f-4c96-9966-49306b00ca90','Lyubo','lyubo@me.com','$2y$12$w5Bs/vKa0D1uEKlo6AH8QexQujTjtSRIecBEwkSyo/nRzkR4KOdHO','https://github.com/impzero.png', 5000, "Admin", 1);
+INSERT INTO `shops` VALUES
+    (1, "Paconi"),
+    (2, "Lidl");
+INSERT INTO `shop_owners` VALUES
+    (1, '24a2068c-a43d-43ae-b967-858953686189'),
+    (2, '1cd005fe-213f-4c96-9966-49306b00ca90');
 INSERT INTO `products` VALUES 
     (1,'Akai 32-inch HD LED LCD',500,'https://azcd.harveynorman.com.au/media/catalog/product/cache/21/image/992x558/9df78eab33525d08d6e5fb8d27136e95/a/k/ak3219nf.jpg',"Enjoy watching your favourite movies and shows in stunning HD quality with the Akai 32-inch HD LED LCD Smart TV.",5, 2),
     (2,'CYBERPOWERPC Gamer Master Gaming PC',500,'https://images-na.ssl-images-amazon.com/images/I/812kz16Md0L._SX466_.jpg',"Cyber PowerPC Gamer Master series is a line of gaming PCs powered by AMD's newest Ryzen CPU and accompanying AM4 architecture.",5, 2),
@@ -14,9 +20,3 @@ INSERT INTO `orders` VALUES
     ('4f2e6fea-83f7-49d1-8be2-a3ddcaea0dcb', '19274de1-ac2b-4dd9-adee-20f4ab69e920', 1),
     ('4b75ddf3-334d-4120-9a41-1ced15f0d49c', '19274de1-ac2b-4dd9-adee-20f4ab69e920', 3),
     ('1f7d1e74-b37b-4537-8fcb-756911326249', '1cd005fe-213f-4c96-9966-49306b00ca90', 1);
-INSERT INTO `shops` VALUES
-    (1, "Paconi"),
-    (2, "Lidl");
-INSERT INTO `shop_owners` VALUES
-    (1, '24a2068c-a43d-43ae-b967-858953686189'),
-    (2, '1cd005fe-213f-4c96-9966-49306b00ca90');
--- mysql/schema.go
+++ mysql/schema.go
@@ -14,20 +14,27 @@ CREATE TABLE IF NOT EXISTS users (
   UNIQUE KEY email (email)
 );
 
+CREATE TABLE IF NOT EXISTS shops (
+  shop_id int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name varchar(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS shop_owners (
+  shop_id int(10) unsigned NOT NULL,
+  user_id varchar(36) NOT NULL,
+  FOREIGN KEY (shop_id) REFERENCES shops(shop_id) ON DELETE CASCADE ON UPDATE NO ACTION,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE NO ACTION
+);
+
 CREATE TABLE IF NOT EXISTS products (
   id int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name varchar(255) DEFAULT NULL,
   price double DEFAULT NULL,
   image varchar(255) DEFAULT NULL,
-  description varchar (255) DEFAULT NULL,
-  stock int(10) unsigned DEFAULT NULL
-);
-
-CREATE TABLE IF NOT EXISTS receipts (
-  receipt_id varchar(36) NOT NULL PRIMARY KEY,
-  content TEXT NOT NULL,
-  user_id varchar(36) NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+  description varchar(255) DEFAULT NULL,
+  stock int(10) unsigned DEFAULT NULL,
+  shop_id int(10) unsigned NOT NULL,
+  FOREIGN KEY (shop_id) REFERENCES shops(shop_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS orders (
@@ -36,53 +43,14 @@ CREATE TABLE IF NOT EXISTS orders (
   product_id int(10) unsigned NOT NULL,
   FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE NO ACTION ON UPDATE NO ACTION,
   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE NO ACTION
-	name varchar(255) NOT NULL,
-  email varchar(255) NOT NULL,
-	password varchar(255) NOT NULL,
-	avatar varchar(255) NOT NULL,
-  points double NOT NULL,
-	role varchar(255) NOT NULL,
-	UNIQUE KEY email (email)
-);
-
-CREATE TABLE IF NOT EXISTS shops (
-  shop_id int (10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name varchar(255) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS shop_owners (
-  shop_id int(10) unsigned NOT NULL,
-  user_id int(10) unsigned NOt NULL,
-  FOREIGN KEY (shop_id) REFERENCES shops(shop_id) ON DELETE CASCADE ON UPDATE NO ACTION,
-  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE NO ACTION
-);
-
-CREATE TABLE IF NOT EXISTS products (
-  id int (10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name varchar(255) DEFAULT NULL,
-  price double DEFAULT NULL,
-  image varchar(255) DEFAULT NULL,
-  description varchar (255) DEFAULT NULL,
-  stock int (10) unsigned DEFAULT NULL,
-  shop_id int(10) unsigned NOT NULL,
-  FOREIGN KEY (shop_id) REFERENCES shops(shop_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS receipts (
-  receipt_id int (10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  receipt_id varchar(36) NOT NULL PRIMARY KEY,
   content TEXT NOT NULL,
-  user_id int(10) unsigned NULL,
+  user_id varchar(36) NULL,
   shop_id int(10) unsigned NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-  FOREIGN KEY (shop_id) REFERENCES shops(shop_id) ON DELETE CASCADE
+  FOREIGN KEY (shop_id) REFERENCES shops(shop_id) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
-
-CREATE TABLE IF NOT EXISTS orders (
-  order_id int (10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  user_id int (10) unsigned NOT NULL,
-  product_id int (10) unsigned NOT NULL,
-  FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE NO ACTION
-);
-
 `
