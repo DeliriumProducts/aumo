0ee70bd731beee8f04c1cf21132dc5a2f6025239
Simo Aleksandrov <simo3003@me.com>
2020-02-15 00:37:14 +0200

    circle using neomoprh

--- client/screens/app/home.js
+++ client/screens/app/home.js
@@ -1,7 +1,9 @@
-import { Button, Text } from "@ui-kitten/components"
+import { Icon } from "@ui-kitten/components"
 import React from "react"
-import { View } from "react-native"
+import { Image, TouchableWithoutFeedback, View } from "react-native"
+import { NeomorphBox } from "react-native-neomorph-shadows"
 import NfcManager, { NdefParser, NfcEvents } from "react-native-nfc-manager"
+import theme from "../../theme"
 
 export default () => {
   React.useEffect(() => {
@@ -18,9 +20,15 @@ export default () => {
   }, [])
 
   return (
-    <View>
-      <Text>aumo!</Text>
-      <Button
+    <View style={{ justifyContent: "center", alignItems: "center" }}>
+      <Image
+        source={require("../../assets/AumoLogo.png")}
+        style={{
+          width: 220,
+          resizeMode: "contain"
+        }}
+      />
+      <TouchableWithoutFeedback
         onPress={async () => {
           try {
             await NfcManager.registerTagEvent()
@@ -30,8 +38,50 @@ export default () => {
           }
         }}
       >
-        Click to scan!
-      </Button>
+        <NeomorphBox
+          style={{
+            shadowRadius: 3,
+            borderRadius: 120,
+            backgroundColor: "#fafafa",
+            width: 240,
+            height: 240,
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <NeomorphBox
+            inner
+            style={{
+              shadowRadius: 7,
+              borderRadius: 100,
+              backgroundColor: theme["color-primary-100"],
+              width: 200,
+              height: 200,
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+          >
+            <NeomorphBox
+              style={{
+                shadowRadius: 7,
+                borderRadius: 50,
+                backgroundColor: "#fafafa",
+                width: 100,
+                height: 100,
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <Icon
+                name="wifi-outline"
+                width={60}
+                height={60}
+                fill={theme["color-primary-500"]}
+              />
+            </NeomorphBox>
+          </NeomorphBox>
+        </NeomorphBox>
+      </TouchableWithoutFeedback>
     </View>
   )
 }
