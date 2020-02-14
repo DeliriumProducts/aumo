d8d4ea723531e39e7fc186455096983e2cf2ea09
Simo Aleksandrov <simo3003@me.com>
2020-02-14 16:59:24 +0200

    use dp's fork for the react native animated tab abr

--- client/navigation/app/app.js
+++ client/navigation/app/app.js
@@ -1,7 +1,7 @@
+import { TabBar } from "@deliriumproducts/react-native-animated-nav-tab-bar"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import { Avatar, Icon } from "@ui-kitten/components"
 import React from "react"
-import { TabBar } from "react-native-animated-nav-tab-bar"
 import { Context } from "../../context/context"
 import theme from "../../theme"
 import Routes from "../routes"
