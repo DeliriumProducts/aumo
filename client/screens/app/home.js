62414cd442f94abc655a42d6ebe2e54675046ba4
Simo Aleksandrov <simo3003@me.com>
2020-02-15 00:19:37 +0200

    really basic nfc

--- client/screens/app/home.js
+++ client/screens/app/home.js
@@ -1,6 +1,37 @@
-import { Text } from "@ui-kitten/components"
+import { Button, Text } from "@ui-kitten/components"
 import React from "react"
+import { View } from "react-native"
+import NfcManager, { NdefParser, NfcEvents } from "react-native-nfc-manager"
 
 export default () => {
-  return <Text>ok!</Text>
+  React.useEffect(() => {
+    NfcManager.start()
+    NfcManager.setEventListener(NfcEvents.DiscoverTag, tag => {
+      let msgs = tag.ndefMessage.map(NdefParser.parseText)
+      console.warn(msgs)
+    })
+
+    return () => {
+      NfcManager.setEventListener(NfcEvents.DiscoverTag, null)
+      NfcManager.unregisterTagEvent().catch(() => 0)
+    }
+  }, [])
+
+  return (
+    <View>
+      <Text>aumo!</Text>
+      <Button
+        onPress={async () => {
+          try {
+            await NfcManager.registerTagEvent()
+          } catch (error) {
+            console.warn(error)
+            NfcManager.unregisterTagEvent().catch(() => 0)
+          }
+        }}
+      >
+        Click to scan!
+      </Button>
+    </View>
+  )
 }
