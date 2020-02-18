e9072d4d16761e4c7b6cfe4a614733b2227330b9
Simo Aleksandrov <simo3003@me.com>
2020-02-19 00:10:27 +0200

    padding / margin

--- client/components/ProductList.js
+++ client/components/ProductList.js
@@ -1,67 +1,86 @@
-import { Button, Card, Icon, List, Text } from "@ui-kitten/components"
+import { Button, Icon, List, Text } from "@ui-kitten/components"
 import React from "react"
-import { Dimensions, ImageBackground, View } from "react-native"
+import { Dimensions, Image, TouchableOpacity, View } from "react-native"
 export default ({
   onItemPress = () => {},
   onCartPress = () => {},
   products
 }) => {
   const renderProductItem = ({ item }) => (
-    <Card
+    <TouchableOpacity
       style={{
         flex: 1,
-        margin: 8,
+        margin: 3,
         borderRadius: 16,
-        maxWidth: Dimensions.get("window").width / 2 - 24
+        maxWidth: Dimensions.get("window").width / 2 - 24,
+        padding: 10,
+        minHeight: 250
       }}
-      appearance="filled"
-      header={() => (
-        <ImageBackground
-          style={{ height: 140 }}
+      onPress={() => onItemPress(item)}
+    >
+      <View
+        style={{
+          height: "100%",
+          backgroundColor: "#fff",
+          borderRadius: 16,
+          padding: 6
+        }}
+      >
+        <Image
+          style={{ height: 80 }}
           source={{ uri: item.image }}
           resizeMode="contain"
         />
-      )}
-      footer={() => (
         <View
           style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center"
+            flex: 1,
+            padding: 16,
+            justifyContent: "space-between"
           }}
         >
+          <View>
+            <Text category="s1">{item.name}</Text>
+          </View>
           <View
             style={{
               flexDirection: "row",
-              justifyContent: "center",
-              alignItems: "center"
+              alignItems: "center",
+              justifyContent: "space-between"
             }}
           >
-            <Icon name="award-outline" width={17} height={17} fill="#222" />
-            <Text
-              category="s1"
+            <View
               style={{
-                marginLeft: 3
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center"
               }}
             >
-              {item.price}
-            </Text>
+              <Icon
+                name="award-outline"
+                width={17}
+                height={17}
+                fill="#8f9bb3"
+              />
+              <Text
+                category="s1"
+                appearance="hint"
+                style={{
+                  marginLeft: 3
+                }}
+              >
+                {item.price}
+              </Text>
+            </View>
+            <Button
+              style={{ paddingHorizontal: 0 }}
+              size="small"
+              icon={style => <Icon {...style} name="shopping-cart" />}
+              onPress={() => onCartPress(item)}
+            />
           </View>
-          <Button
-            style={{ paddingHorizontal: 0 }}
-            size="small"
-            icon={style => <Icon {...style} name="shopping-cart" />}
-            onPress={() => onCartPress(item)}
-          />
         </View>
-      )}
-      onPress={() => onItemPress(item)}
-    >
-      <Text category="s1">{item.name}</Text>
-      {/* <Text appearance="hint" category="c1">
-        {info.item.category}
-      </Text> */}
-    </Card>
+      </View>
+    </TouchableOpacity>
   )
 
   return (
