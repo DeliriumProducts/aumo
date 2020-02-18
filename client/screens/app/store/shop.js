6ca6f37229172fed3a973f736e101b033953b0af
Simo Aleksandrov <simo3003@me.com>
2020-02-19 00:34:44 +0200

    implement order placement

--- client/screens/app/store/shop.js
+++ client/screens/app/store/shop.js
@@ -1,10 +1,13 @@
-import { Layout, Spinner } from "@ui-kitten/components"
+import { Layout, Modal, Spinner } from "@ui-kitten/components"
 import aumo from "aumo"
 import React from "react"
+import { Alert } from "react-native"
 import styled from "styled-components/native"
 import ProductList from "../../../components/ProductList"
+import { Context } from "../../../context/context"
 
 export default ({ route }) => {
+  const ctx = React.useContext(Context)
   const [products, setProducts] = React.useState([])
   const [loading, setLoading] = React.useState(true)
 
@@ -21,24 +24,52 @@ export default ({ route }) => {
     })()
   }, [])
 
+  const placeOrder = async product => {
+    try {
+      setLoading(true)
+      await aumo.order.placeOrder({
+        product_id: product.id
+      })
+      Alert.alert("Successfull!", "You successfully purchased " + product.name)
+    } catch (error) {
+      Alert.alert("Error!", error.response.data.error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <Layout style={{ height: "100%" }} level="1">
-      <ProductList products={products} />
+      <ProductList
+        products={products.map(p => ({
+          ...p,
+          disabled: p.stock < 1 || ctx.state.user.points < p.price
+        }))}
+        onCartPress={product => {
+          Alert.alert(
+            "Purchase confirmation",
+            `Would you want to order ${product.name}?`,
+            [
+              {
+                text: "Yes",
+                onPress: () => placeOrder(product)
+              },
+              {
+                text: "Cancel",
+                onPress: () => {},
+                style: "cancel"
+              }
+            ],
+            { cancelable: true }
+          )
+        }}
+      />
       {loading && (
-        <Layout
-          level="2"
-          style={{
-            height: "100%",
-            width: "100%",
-            position: "absolute",
-            justifyContent: "center",
-            alignItems: "center"
-          }}
-        >
+        <Modal onBackdropPress={() => {}} visible={loading}>
           <ModalContainer level="1">
             <Spinner size="giant" />
           </ModalContainer>
-        </Layout>
+        </Modal>
       )}
     </Layout>
   )
