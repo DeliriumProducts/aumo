38f444b3a8e2c2a10548a1ad9367ec35432bd108
Simo Aleksandrov <simo3003@me.com>
2020-02-21 13:21:07 +0200

    show price in receiptlist and add onpress handler

--- client/components/ReceiptList.js
+++ client/components/ReceiptList.js
@@ -1,12 +1,13 @@
 import { Avatar, Icon, List, ListItem } from "@ui-kitten/components"
 import React from "react"
 
-export default ({ receipts }) => {
+export default ({ receipts, onItemPress = () => {} }) => {
   const renderItem = ({ item: receipt }) => (
     <ListItem
       style={{ borderRadius: 16, margin: 5 }}
       title={receipt.shop.name}
-      description={receipt.content}
+      description={receipt.total}
+      onPress={() => onItemPress(receipt)}
       icon={styles => <Icon name="shopping-bag-outline" {...styles} />}
       accessory={styles => (
         <Avatar source={{ uri: receipt.shop.image }} {...styles} size="small" />
