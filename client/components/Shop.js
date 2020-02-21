a0d37dae72d104417d6da4b5738c22affcf84e17
Simo Aleksandrov <simo3003@me.com>
2020-02-21 13:31:51 +0200

    receipt details screen

--- client/components/Shop.js
+++ client/components/Shop.js
@@ -2,14 +2,13 @@ import { Text } from "@ui-kitten/components"
 import React from "react"
 import { ImageBackground, TouchableOpacity } from "react-native"
 
-export default ({ name, onPress, image, style }) => (
+export default ({ name, onPress = () => {}, image, style }) => (
   <TouchableOpacity onPress={onPress}>
     <ImageBackground
       source={{ uri: image }}
       blurRadius={8}
       style={[style, { justifyContent: "center", alignItems: "center" }]}
-      imageStyle={{ borderRadius: 16 }}
-    >
+      imageStyle={{ borderRadius: 16 }}>
       <Text category="h1" style={{ color: "white", fontWeight: "bold" }}>
         {name}
       </Text>
---
+++ client/screens/app/user/receipt.js
@@ -0,0 +1,88 @@
+import { Button, Icon, Layout, Text } from "@ui-kitten/components"
+import React from "react"
+import { ImageBackground, ScrollView, View } from "react-native"
+import styled from "styled-components/native"
+import theme from "../../../theme"
+
+export default ({
+  route: {
+    params: { receipt }
+  }
+}) => {
+  return (
+    <Header>
+      <Shop
+        name={receipt.shop.name}
+        image={{ uri: receipt.shop.image }}
+        style={{
+          height: 340,
+          width: "100%"
+        }}
+      />
+      <DetailsContainer level="1">
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            flexWrap: "wrap"
+          }}>
+          <View>
+            <Text category="h4">{receipt.shop.name}</Text>
+          </View>
+          <View style={{ alignItems: "center" }}>
+            <Price>
+              <Icon
+                name="award-outline"
+                width={25}
+                height={25}
+                fill={theme["color-basic-800"]}
+              />
+              <Text category="h4">{receipt.total}</Text>
+            </Price>
+          </View>
+        </View>
+        <Content appearance="hint">{receipt.content}</Content>
+      </DetailsContainer>
+    </Header>
+  )
+}
+
+const Header = styled(ScrollView)`
+  min-height: 100%;
+`
+const ProductImage = styled(ImageBackground)``
+const DetailsContainer = styled(Layout)`
+  padding-vertical: 24px;
+  padding-horizontal: 16px;
+`
+
+const Shop = styled(Text)`
+  margin-top: 4px;
+`
+
+const Price = styled(View)`
+  flex-direction: row;
+  align-items: center;
+`
+
+const Content = styled(Text)`
+  margin-vertical: 16px;
+`
+
+const ActionContainer = styled(View)`
+  flex-direction: row;
+  margin-horizontal: -8px;
+  margin-top: 24px;
+`
+
+const ActionButton = styled(Button)`
+  flex: 1;
+  margin-horizontal: 8px;
+`
+
+const ModalContainer = styled(Layout)`
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  padding: 16px;
+`
