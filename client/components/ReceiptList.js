7b269e26cd2ee4148bf529057ab14b8e93389da1
Lyubo Lyubchev <lyubo_2317@abv.bg>
2020-02-22 15:56:30 +0200

    remove clog

--- client/components/ReceiptList.js
+++ client/components/ReceiptList.js
@@ -2,23 +2,18 @@ import { Avatar, Icon, List, ListItem } from "@ui-kitten/components"
 import React from "react"
 
 export default ({ receipts, onItemPress = () => {} }) => {
-  const renderItem = ({ item: receipt }) =>
-    console.log(receipt) || (
-      <ListItem
-        style={{ borderRadius: 16, margin: 5 }}
-        title={receipt.shop.name}
-        description={receipt.total}
-        onPress={() => onItemPress(receipt)}
-        icon={styles => <Icon name="shopping-bag-outline" {...styles} />}
-        accessory={styles => (
-          <Avatar
-            source={{ uri: receipt.shop.image }}
-            {...styles}
-            size="small"
-          />
-        )}
-      />
-    )
+  const renderItem = ({ item: receipt }) => (
+    <ListItem
+      style={{ borderRadius: 16, margin: 5 }}
+      title={receipt.shop.name}
+      description={receipt.total}
+      onPress={() => onItemPress(receipt)}
+      icon={styles => <Icon name="shopping-bag-outline" {...styles} />}
+      accessory={styles => (
+        <Avatar source={{ uri: receipt.shop.image }} {...styles} size="small" />
+      )}
+    />
+  )
 
   return <List data={receipts} renderItem={renderItem} />
 }
